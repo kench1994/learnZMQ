07e3b4f48afb19433b7bd573572f377ea027baef
kench1994 <kench.h.wong@gmail.com>
2022-03-22 09:51:15 +0800

    wait for abstract

--- asyncs.cpp
+++ asyncs.cpp
@@ -4,6 +4,7 @@
 #include "czmq.h"
 #include <thread>
 #include <functional>
+#include <cstdlib>
 //  ---------------------------------------------------------------------
 //  这是client端任务，它会连接至server，每秒发送一次请求，同时收集和打印应答消息。
 //  我们会运行多个client端任务，使用随机的标识。
@@ -15,22 +16,20 @@ client_task(void* args)
     //  设置随机标识，方便跟踪
     char identity[10];
     sprintf(identity, "%04X-%04X", randof(0x10000), randof(0x10000));
-	zmq_setsockopt(client, ZMQ_IDENTITY, "A", 1);
+    zmq_setsockopt(client, ZMQ_IDENTITY, "A", 1);
     zmq_connect(client, "tcp://localhost:5570");
     zmq_pollitem_t items[] = { { client, 0, ZMQ_POLLIN, 0 } };
     int request_nbr = 0;
+	zstr_sendf(client, "request #%d", ++request_nbr);
     while (1) {
-        //  从poll中获取消息，每秒一次
-        int centitick;
-        for (centitick = 0; centitick < 100; centitick++) {
-            zmq_poll(items, 1, 10 * ZMQ_POLL_MSEC);
+        {
+            zmq_poll(items, 1, -1);
             if (items[0].revents & ZMQ_POLLIN) {
                 zmsg_t* msg = zmsg_recv(client);
-                zframe_print(zmsg_last(msg), identity);
+                zframe_print(zmsg_last(msg), "client recv");
                 zmsg_destroy(&msg);
             }
         }
-        zstr_sendf(client, "request #%d", ++request_nbr);
     }
     zmq_term(&ctx);
     return NULL;
@@ -50,10 +49,10 @@ void* server_task(void* args)
     zmq_bind(backend, "inproc://backend");
     //  启动一个worker线程池，数量任意
     int thread_nbr;
-    for (thread_nbr = 0; thread_nbr < 5; thread_nbr++)
+    //for (thread_nbr = 0; thread_nbr < 5; thread_nbr++)
     {
-		auto* pTrd = new std::thread(std::bind(&server_worker, (void*)NULL, ctx, (void*)NULL));
-		pTrd->detach();
+        auto* pTrd = new std::thread(std::bind(&server_worker, (void*)NULL, ctx, (void*)NULL));
+        pTrd->detach();
     }
     //    zthread_fork(ctx, server_worker, NULL);
     //  使用队列装置连接backend和frontend，我们本来可以这样做：
@@ -68,14 +67,12 @@ void* server_task(void* args)
         zmq_poll(items, 2, -1);
         if (items[0].revents & ZMQ_POLLIN) {
             zmsg_t* msg = zmsg_recv(frontend);
-            //puts ("Request from client:");
-            //zmsg_dump (msg);
+
             zmsg_send(&msg, backend);
         }
         if (items[1].revents & ZMQ_POLLIN) {
             zmsg_t* msg = zmsg_recv(backend);
-            //puts ("Reply from worker:");
-            //zmsg_dump (msg);
+
             zmsg_send(&msg, frontend);
         }
     }
@@ -92,18 +89,16 @@ server_worker(void* args, void* ctx, void* pipe)
     while (1) {
         //  DEALER套接字将信封和消息内容一起返回给我们
         zmsg_t* msg = zmsg_recv(worker);
+		zframe_print(zmsg_last(msg), "server recv");
+        //fprintf(stdout, zmsg_dump());
         zframe_t* address = zmsg_pop(msg);
         zframe_t* content = zmsg_pop(msg);
         assert(content);
         zmsg_destroy(&msg);
-        //  随机返回0至4条应答
-        int reply, replies = randof(5);
-        for (reply = 0; reply < replies; reply++) {
-            //  暂停一段时间
-            zclock_sleep(randof(1000) + 1);
-            zframe_send(&address, worker, ZFRAME_REUSE + ZFRAME_MORE);
-            zframe_send(&content, worker, ZFRAME_REUSE);
-        }
+
+		zframe_send(&address, worker, ZFRAME_REUSE + ZFRAME_MORE);
+		zframe_send(&content, worker, ZFRAME_REUSE);
+
         zframe_destroy(&address);
         zframe_destroy(&content);
     }
@@ -113,9 +108,10 @@ server_worker(void* args, void* ctx, void* pipe)
 int main(void)
 {
     auto* ctx = zmq_ctx_new();
-    
-	auto* pTrd = new std::thread(std::bind(&server_task, (void*)NULL));
-	pTrd->detach();
+
+    auto* pTrd = new std::thread(std::bind(&server_task, (void*)NULL));
+    pTrd->detach();
+	zclock_sleep(1000);
 
     for (int i = 0; i < 4; i++)
     {
